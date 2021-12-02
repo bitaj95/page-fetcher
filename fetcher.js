dce39d0b735711334babd92bab1351ee14b17f19
Bita <bita.janzadeh@hotmail.com>
2021-12-02 00:41:45 -0500

    first draft

---
+++ fetcher.js
@@ -0,0 +1,21 @@
+const request = require('request');
+const fs = require('fs');
+
+const args = process.argv.slice(2);
+const URL = args[0];
+const localFilePath = args[1];
+
+request(URL, (error, response, body) => {
+
+  if (error) {
+    console.log('error:', error); 
+    return;
+  }
+  fs.writeFile(localFilePath, body, err => {
+    if (err) {
+      console.error(err)
+      return
+    }
+    console.log(`Downloaded and saved ${body.length} bytes to ${localFilePath}`);
+  });
+});
